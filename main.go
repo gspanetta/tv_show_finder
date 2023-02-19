86f46e10845debf29a965118708f6b977f7604b5
Gianluca Panetta <g@p>
2023-02-19 01:05:08 +0100

    implement functions for every stage of user interaction

--- main.go
+++ main.go
@@ -54,13 +54,11 @@ func getUserInputStr(prompt string) string {
 }
 
 func getUserInputNum(prompt string) (int, error) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println()
-	fmt.Println(prompt)
-	fmt.Print("> ")
-	query, _ := reader.ReadString('\n')
-	query = strings.Replace(query, "\r\n", "", -1)
-	query = strings.Replace(query, "\n", "", -1)
+	query := getUserInputStr(prompt)
+
+	if strings.Compare("q", query) == 0 {
+		os.Exit(0)
+	}
 
 	num, err := strconv.Atoi(query)
 	if err != nil {
@@ -89,7 +87,6 @@ func httpRequest(uri string, sink interface{}) error {
 		log.Fatalln(err)
 	}
 
-	//var queryResponse TmdbQueryResponse
 	err = json.Unmarshal(body, sink)
 	if err != nil {
 		log.Fatalln(err)
@@ -98,92 +95,158 @@ func httpRequest(uri string, sink interface{}) error {
 	return nil
 }
 
+var api_key string
+var show_list []TmdbQueryResponseResults
+var num_of_seasons int
+var strSelectedSeason string
+var show_id int
+
+const (
+	stateUserQuery         int = 0
+	stateUserSelectShow        = 1
+	stateUserSelectSeason      = 2
+	stateUserSelectEpisode     = 3
+)
+
+var currentState int
+var nextState int
+
+func userQuery() {
+	// wait for user input
+	query := getUserInputStr("Search for TV show (enter keywords or type 'q' to exit)")
+
+	// quit program when "q" is entered, restart when nothing is entered
+	if strings.Compare("q", query) == 0 {
+		fmt.Println("Goodbye!")
+		os.Exit(0)
+	} else if strings.Compare("", query) == 0 {
+		return
+	}
+
+	var queryResponse TmdbQueryResponse
+	httpRequest("https://api.themoviedb.org/3/search/tv?api_key="+api_key+"&query="+url.QueryEscape(query)+"&include_adult=false", &queryResponse)
+	show_list = queryResponse.Results
+
+	if len(show_list) == 0 {
+		fmt.Println("Didn't find anything. Please try searching with other keywords.")
+		return
+	}
+
+	if queryResponse.Total_pages > 1 {
+		fmt.Println(" -- more than 20 entries found, please add more keywords --")
+	}
+
+	nextState = stateUserSelectShow
+}
+
+func userSelectShow() {
+
+	// display all shows found
+	for show := range show_list {
+		fmt.Println(show+1, ": ", show_list[show].Name)
+	}
+
+	// let user select a show
+	selectedShow, err := getUserInputNum("Select TV show")
+
+	if err != nil {
+		fmt.Println("No valid input")
+		return
+	}
+
+	if selectedShow > 20 || selectedShow < 1 {
+		fmt.Println("No valid selection")
+		return
+	}
+
+	show_id = show_list[selectedShow-1].Id
+
+	// get all seasons
+	var getDetailsResponse TmdbTvGetDetailsResponse
+	httpRequest("https://api.themoviedb.org/3/tv/"+strconv.Itoa(show_id)+"?api_key="+api_key, &getDetailsResponse)
+
+	num_of_seasons = getDetailsResponse.Number_of_seasons
+
+	nextState = stateUserSelectSeason
+}
+
+func userSelectSeason() {
+	// let user select a season
+	// read user input as number and convert to string afterwards, so we get an error when the user does not enter a number
+	selectedSeason, err := getUserInputNum("Select Season 1 to " + strconv.Itoa(num_of_seasons))
+
+	if err != nil {
+		fmt.Println("No valid input")
+		return
+	}
+
+	if selectedSeason < 0 && selectedSeason > num_of_seasons {
+		fmt.Println("No valid selection!")
+		return
+	}
+
+	strSelectedSeason = strconv.Itoa(selectedSeason)
+
+	nextState = stateUserSelectEpisode
+}
+
+func userSelectEpisode() {
+	// get episodes of the selected season
+	var getSeasonDetailsResponse TmdbTvGetSeasonDetailsResponse
+	httpRequest("https://api.themoviedb.org/3/tv/"+strconv.Itoa(show_id)+"/season/"+strSelectedSeason+"?api_key="+api_key, &getSeasonDetailsResponse)
+
+	// display list of episodes
+	fmt.Println("Episodes from Season ", strSelectedSeason, ":")
+
+	for episode := range getSeasonDetailsResponse.Episodes {
+		fmt.Println(getSeasonDetailsResponse.Episodes[episode].Episode_number, " - ", getSeasonDetailsResponse.Episodes[episode].Name)
+	}
+
+	// let user select episode
+	selectedEpisode, err := getUserInputNum("Select Episode 1 to " + strconv.Itoa(len(getSeasonDetailsResponse.Episodes)))
+
+	if err != nil {
+		fmt.Println("No valid selection")
+	}
+
+	if selectedEpisode < 1 || selectedEpisode > len(getSeasonDetailsResponse.Episodes) {
+		fmt.Println("there's no such episode!")
+		return
+	}
+
+	// display name and overview of the selected episode
+	fmt.Println()
+	fmt.Println("Overview of Season ", strSelectedSeason, " Episode ", strconv.Itoa(selectedEpisode), ": ", getSeasonDetailsResponse.Episodes[selectedEpisode-1].Name)
+	fmt.Println("   ", getSeasonDetailsResponse.Episodes[selectedEpisode-1].Overview)
+
+	nextState = stateUserQuery
+}
+
 func main() {
 	// get TMDB API key from environment variable
-	api_key := os.Getenv("TMDB_API_KEY")
+	api_key = os.Getenv("TMDB_API_KEY")
 	if api_key == "" {
 		fmt.Println("Please set TMDB_API_KEY environment variable")
 		return
 	}
 
+	currentState = stateUserQuery
+	nextState = stateUserQuery
+
 	for {
-		// wait for user input
-		query := getUserInputStr("Search for TV show (enter keywords or type 'q' to exit)")
-
-		// quit program when "q" is entered, restart when nothing is entered
-		if strings.Compare("q", query) == 0 {
-			fmt.Println("Goodbye!")
-			os.Exit(0)
-		} else if strings.Compare("", query) == 0 {
-			continue
+		switch currentState {
+		case stateUserQuery:
+			userQuery()
+		case stateUserSelectShow:
+			userSelectShow()
+		case stateUserSelectSeason:
+			userSelectSeason()
+		case stateUserSelectEpisode:
+			userSelectEpisode()
 		}
 
-		var queryResponse TmdbQueryResponse
-		httpRequest("https://api.themoviedb.org/3/search/tv?api_key="+api_key+"&query="+url.QueryEscape(query)+"&include_adult=false", &queryResponse)
-
-		show_list := queryResponse.Results
-
-		// display all shows found
-		for show := range show_list {
-			fmt.Println(show+1, ": ", show_list[show].Name)
+		if nextState != currentState {
+			currentState = nextState
 		}
-
-		if queryResponse.Total_pages > 1 {
-			fmt.Println(" -- more than 20 entries found, please add more keywords --")
-		}
-
-		// let user select a show
-		selectedShow, _ := getUserInputNum("Select TV show")
-
-		if selectedShow == 0 {
-			continue
-		} else if selectedShow > 20 || selectedShow < 0 {
-			fmt.Println("No valid selection")
-			continue
-		}
-
-		show_id := show_list[selectedShow-1].Id
-
-		// get all seasons
-		var getDetailsResponse TmdbTvGetDetailsResponse
-		httpRequest("https://api.themoviedb.org/3/tv/"+strconv.Itoa(show_id)+"?api_key="+api_key, &getDetailsResponse)
-
-		num_of_seasons := getDetailsResponse.Number_of_seasons
-
-		// let user select a season
-		// read user input as number and convert to string afterwards, so we get an error when the user does not enter a number
-		selectedSeason, _ := getUserInputNum("Select Season 1 to " + strconv.Itoa(num_of_seasons))
-		if selectedSeason < 0 && selectedSeason > num_of_seasons {
-			fmt.Println("No valid selection!")
-			continue
-			// TODO: go back to prompt
-		}
-
-		strSelectedSeason := strconv.Itoa(selectedSeason)
-
-		// get episodes of the selected season
-		var getSeasonDetailsResponse TmdbTvGetSeasonDetailsResponse
-		httpRequest("https://api.themoviedb.org/3/tv/"+strconv.Itoa(show_id)+"/season/"+strSelectedSeason+"?api_key="+api_key, &getSeasonDetailsResponse)
-
-		// display list of episodes
-		fmt.Println("Episodes from Season ", strSelectedSeason, ":")
-
-		for episode := range getSeasonDetailsResponse.Episodes {
-			fmt.Println(getSeasonDetailsResponse.Episodes[episode].Episode_number, " - ", getSeasonDetailsResponse.Episodes[episode].Name)
-		}
-
-		// let user select episode
-		selectedEpisode, _ := getUserInputNum("Select Episode 1 to " + strconv.Itoa(len(getSeasonDetailsResponse.Episodes)))
-
-		if selectedEpisode < 1 || selectedEpisode > len(getSeasonDetailsResponse.Episodes) {
-			fmt.Println("there's no such episode!")
-			continue
-		}
-
-		// display name and overview of the selected episode
-		fmt.Println()
-		fmt.Println("Overview of Season ", strSelectedSeason, " Episode ", strconv.Itoa(selectedEpisode), ": ", getSeasonDetailsResponse.Episodes[selectedEpisode-1].Name)
-		fmt.Println("   ", getSeasonDetailsResponse.Episodes[selectedEpisode-1].Overview)
-
 	}
 }
